e6d29722814793d1941e8c1b9dc2f1793f99c180
limitimil <zlsh10340@gmail.com>
2020-01-23 19:02:28 +0800

    Config: for vue-lint feature

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 config = [
         {
-        'repository_url':'http://tfs.cybersoft4u.com.tw:8080/tfs/SDD/TIS/_git/CyberSoft.MiniTis.Web',
-        'branches': ['test']
-        },
-        {
-        'repository_url':'http://tfs.cybersoft4u.com.tw:8080/tfs/SDD/TIS/_git/CyberSoft.MiniTis.Web',
-        'branches': ['test2']
+                'repository_url':'http://tfs.cybersoft4u.com.tw:8080/tfs/SDD/TIS/_git/CyberSoft.MiniTis.Web',
+                'branches': ['rebase-helper-test-branch'],
+                'plugin_actions': {
+                        'vue-lint':{
+                                'name': 'vue-lint',
+                                'path': 'CyberSoft.MiniTis.Web',
+                        }
+                }
         },
 ]
