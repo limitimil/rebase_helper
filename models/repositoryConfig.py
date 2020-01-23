8a53b4d884eb2f80ca400e7091e6538b7ae49861
limitimil <zlsh10340@gmail.com>
2020-01-23 20:28:07 +0800

    HANDLER: use vue-lint-controller in before_push stage

--- models/repositoryConfig.py
+++ models/repositoryConfig.py
@@ -1,8 +1,20 @@
+"""
+RepositoryConfig.plugin_actions should be a dictionary. Like:
+{
+    'vue-lint':{
+        'name': 'vue-lint',
+        'path': 'CyberSoft.MiniTis.Web'
+    }
+}
+"""
 class RepositoryConfig:
+
     def __init__(self, config):
         self.url = config['repository_url']
         self.branches = config['branches']
         self.reference_branch = config.get('reference_branch', 'origin/develop')
+        self.plugin_actions = config.get('plugin_actions', None)
+
     def __iter__(self):
         self._iter_pos = 0
         return self
--- rebase_helper.py
+++ rebase_helper.py
@@ -1,18 +1,38 @@
+import os
 import git
 import gitCurator
 import config
 from models.repositoryConfig import RepositoryConfig
+from services.vue_lint_controller import VueLintController
+
 
 class RebaseHandler(gitCurator.GitCurator):
+    def before_rebase(self, metadata):
+        pass
+
+    def before_push(self, metadata):
+        if 'vue-lint' in self.plugin_actions:
+            controller = VueLintController(os.path.join(
+                self.current_workspace, self.plugin_actions['vue-lint']['path']))
+            controller.run()
+            self.current_repo.git.add('-u')
+            self.current_repo.git.commit(
+                '-m', 'npm run lint by automation tool')
+
     def setup_repo(self, metadata):
         self.reference_branch = metadata.reference_branch
+        self.plugin_actions = metadata.plugin_actions
+
     def run(self, metadata):
         commitId = self.current_repo.head.commit
         self.current_repo.git.checkout(self.current_branch)
+        self.before_rebase(metadata)
         self.current_repo.git.rebase(self.reference_branch)
         if commitId == self.current_repo.head.commit or self.current_repo.is_dirty():
             raise Exception("rebase fail")
-        self.current_repo.git.push('origin', self.current_repo.name, '-f')
+        self.before_push(metadata)
+        self.current_repo.git.push('origin', self.current_branch, '-f')
+
 
 if __name__ == '__main__':
     repos = map(lambda x: RepositoryConfig(x), config.config)
