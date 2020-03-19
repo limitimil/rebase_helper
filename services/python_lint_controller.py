f09862eaa60005772b578f533f5d8acea86daacc
limitimil <zlsh10340@gmail.com>
2020-03-19 16:09:01 +0800

    core of python linter

---
+++ services/python_lint_controller.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+from subprocess import Popen
+SCRIPT = " python -m autopep8 -i {file_path} --ignore E402"
+
+class PythonLintController:
+    history = []
+
+    def __init__(self, working_directory):
+        self.working_directory = working_directory
+        self.history = []
+
+    def _get_python_paths(self, root_folder):
+        files = []
+        for r, d, f in os.walk(root_folder):
+            for file in f:
+                if file.endswith('.py'):
+                    files.append(os.path.join(r, file))
+
+        return files
+
+    def run(self, **kwargs):
+        folders = kwargs.get('targets', [])
+        folders = list(map(lambda x: os.path.join(self.working_directory, x), folders))
+        files = []
+        for f in folders:
+            files.extend(self._get_python_paths(f))
+        for f in files:
+            process = Popen(SCRIPT.format(file_path=f),
+                stdout=subprocess.PIPE, shell=True, cwd = self.working_directory
+            )
+            result = process.communicate()
+            self.history.append(result)
+        return self.history
