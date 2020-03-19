423d4934c7f41eaaf139fb3a7cccdbc1cb1a7bf2
limitimil <zlsh10340@gmail.com>
2020-03-19 16:09:50 +0800

    config for test the function of feature-autopep8

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
 config = [
         {
-                'repository_url':'http://tfs.cybersoft4u.com.tw:8080/tfs/SDD/TIS/_git/CyberSoft.MiniTis.Web',
+                'repository_url':'http://tfs.cybersoft4u.com.tw:8080/tfs/SDD/TIS/_git/CloudTisTesting',
                 'branches': [
-                    'rebase-helper-branch',
-                    'bugfix-CTIS-1062',
-                    'feature-CTIS-1348',
-                    'feature-CTIS-1840-future-work',
+                    'tool-CTIS-2143',
                     ],
                 'plugin_actions': {
-                        'vue-lint':{
-                                'name': 'vue-lint',
-                                'path': 'CyberSoft.MiniTis.Web',
+                        'python-lint':{
+                                'name': 'python-lint',
+                                'targets': ['testtools', 'testutils', 'unittests'],
                         }
                 }
         },
