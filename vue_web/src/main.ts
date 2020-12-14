ea5fc280b095dd74bae3814ef54c79a92e81b0fb
liminchien <liminchien@cybersoft4u.com>
2020-12-14 22:07:15 +0800

    many files: install dependencies for vue project
    2 files: install router view and expose rebase single task
    3 files: install view design
    3 files: install bootstrap vue
    2 files: install axios

--- vue_web/src/main.ts
+++ vue_web/src/main.ts
@@ -3,7 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// View UI
+import ViewUI from "view-design";
+import 'view-design/dist/styles/iview.css';
+// Bootstrap
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
 Vue.config.productionTip = false;
+Vue.use(ViewUI);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
--- vue_web/src/router/index.ts
+++ vue_web/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '../views/Home.vue';
+import { RebaseSingleTask } from '@/components/Rebase';
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,14 @@ const routes: RouteConfig[] = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/rebase',
+    name: 'rebase_home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: RebaseSingleTask,
   },
 ];
 
