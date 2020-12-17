80935288cb3191b0b303cd1aeb23cff8346aac1a
limitimil <liminchien@cybersoft4u.com>
2020-12-17 06:29:58 -0800

    FLASK: ENV: update usage and add readme

--- flask_server/script/env.sh
+++ flask_server/script/env.sh
@@ -1,3 +1,7 @@
+# $ source env.sh 
+### To load all naccessary flask environment
+# $ python3 main.py
+### To start server
 SCRIPT_DIR="$( cd "$( dirname ${BASH_SOURCE[0]} )" >/dev/null 2>&1 && pwd )"
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
