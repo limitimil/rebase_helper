132732b99299cac8914847f0987f1460957005a9
limitimil <liminchien@cybersoft4u.com>
2020-12-17 06:10:43 -0800

    VUE PROJ: unittest: test rebase single task

--- vue_web/tests/unit/example.spec.ts
+++ vue_web/tests/unit/example.spec.ts
@@ -1,12 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { RebaseSingleTask } from '@/components/Rebase';
 
-describe('HelloWorld.vue', () => {
+describe('RebaseSingleTask.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = shallowMount(RebaseSingleTask, { });
+    expect(wrapper.text()).toMatch('Single');
   });
 });
