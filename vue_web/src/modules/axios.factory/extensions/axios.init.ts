bf924862ca4393e5b14cbcd1468e9785e02ee092
limitimil <liminchien@cybersoft4u.com>
2020-12-16 23:51:46 -0800

    axios: fix

--- vue_web/src/modules/axios.factory/extensions/axios.init.ts
+++ vue_web/src/modules/axios.factory/extensions/axios.init.ts
@@ -6,42 +6,8 @@ import { AxiosRequestConfig } from 'axios';
 
 export const httpInitRequest = (config: AxiosRequestConfig) => {
 
-  // Base Url
-  config.baseURL = connectionConfigs.VUE_APP_HOST_BACKEND_URL;
+  config.baseURL = "http://192.168.100.133:9007/";
 
-  // API Version
-  if (config.params) {
-    const apiVersion = config.params['api-version'];
-    Object.assign(config.params, {
-      'api-version': apiVersion ? apiVersion : 1,
-    });
-  } else {
-    config.params = { 'api-version': 1 };
-  }
-
-  // Timeout
-  config.timeout = IsTimeoutCustomized(config) ? config.timeout : HTTP_REQUEST_TIMEOUT;  // Milliseconds
-
-  // Set header: content-type
-  config.headers['Content-Type'] = 'application/json';
-
-  // Set header: bearer token
-  if (store.state.user && store.state.user.tokens) {
-    const token = store.state.user.tokens.accessToken;
-    config.headers.Authorization = `Bearer ${token}`;
-  }
 };
 
-
-  /*
-    AxiosRequestConfig default timeout is 0
-    If timeout is not 0, that must be Customized
-  */
-const IsTimeoutCustomized = (requestConfig: AxiosRequestConfig) => {
-  if (requestConfig.timeout === 0) {
-    return false;
-  }
-
-  return true;
-};
 
--- vue_web/src/modules/axios.factory/index.ts
+++ vue_web/src/modules/axios.factory/index.ts
@@ -4,8 +4,6 @@ import { httpInitRequest } from '@/modules/axios.factory/extensions';
 axios.interceptors.request.use(
   (config: AxiosRequestConfig) => {
 
-    const routeName: string | undefined = router.currentRoute.name;
-
     // Initialize the http request
     httpInitRequest(config);
 
