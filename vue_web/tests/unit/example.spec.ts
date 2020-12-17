bc9f752697fb9c378535c50869c3629d80a7d102
limitimil <liminchien@cybersoft4u.com>
2020-12-17 08:10:20 -0800

    UT: wip

--- vue_web/tests/unit/example.spec.ts
+++ vue_web/tests/unit/example.spec.ts
@@ -1,9 +1,17 @@
 import { shallowMount } from '@vue/test-utils';
 import { RebaseSingleTask } from '@/components/Rebase';
 
+jest.mock('@/services/rebase');
 describe('RebaseSingleTask.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('happy path to check element created', () => {
     const wrapper = shallowMount(RebaseSingleTask, { });
     expect(wrapper.text()).toMatch('Single');
+  });
+  it('keyevent and check api calls', () => {
+    const wrapper = shallowMount(RebaseSingleTask, { });
+    wrapper.find('.task-root').trigger('keydown.shift.187');
+    wrapper.find('.task-root').trigger('keydown.shift.187');
+    wrapper.find('.task-root').trigger('keydown.shift.187');
+    wrapper.find('.task-wrapper').trigger('keyup.enter');
   });
 });
