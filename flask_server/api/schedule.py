cd45b97624d3231df49fc7239a51e0a6d54a2158
limitimil <liminchien@cybersoft4u.com>
2020-12-20 06:57:55 -0800

    API: post function with hack

--- flask_server/api/schedule.py
+++ flask_server/api/schedule.py
@@ -4,14 +4,20 @@ from db import TASK
 from flask import jsonify
 from flask import request
 
+# hack
+from config import config
+from db_models.repositoryConfig import RepositoryConfig
 
 class Schedule:
-    @app.route('/api/schedule', methods=['PUT'])
-    def put():
-        return 'shedule put', 200 
-    @app.route('/api/schedule', methods=['POST'])
-    def post():
-        return 'shedule post', 200 
+    @app.route('/api/schedule/<doc_id>', methods=['POST'])
+    @app.route('/api/schedule', methods=['POST'], defaults={'doc_id': None})
+    def post(doc_id):
+        payload = request.json if request.json else config[0]
+        if not doc_id:
+            result = TASK.insert(payload)
+        else:
+            result = TASK.update(payload, doc_ids=[int(doc_id)])
+        return f'shedule put {result}', 200 
     @app.route('/api/schedule', methods=['DELETE'])
     def delete():
         return 'shedule delete', 200 
