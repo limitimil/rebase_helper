ebe1b2fd314470837279f2dbd3cf0d7383d83912
limitimil <liminchien@cybersoft4u.com>
2020-12-20 05:29:13 -0800

    SCH: 4 methods

--- flask_server/api/schedule.py
+++ flask_server/api/schedule.py
@@ -1,9 +1,18 @@
 from __init__ import app
 
 class Schedule:
-    @app.route('/api/schedule/get', methods=['GET'])
-    def get_schedule():
-        return 'shedule', 200 
+    @app.route('/api/schedule', methods=['PUT'])
+    def put():
+        return 'shedule put', 200 
+    @app.route('/api/schedule', methods=['POST'])
+    def post():
+        return 'shedule post', 200 
+    @app.route('/api/schedule', methods=['DELETE'])
+    def delete():
+        return 'shedule delete', 200 
+    @app.route('/api/schedule', methods=['GET'])
+    def get():
+        return 'shedule get', 200 
 
 
 
