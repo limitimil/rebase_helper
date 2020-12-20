9ae3fa918e9dd163566d76dca5e310c97679d425
limitimil <liminchien@cybersoft4u.com>
2020-12-20 07:01:59 -0800

    API: schedule

--- flask_server/api/schedule.py
+++ flask_server/api/schedule.py
@@ -4,26 +4,33 @@ from db import TASK
 from flask import jsonify
 from flask import request
 
-# hack
-from config import config
-from db_models.repositoryConfig import RepositoryConfig
 
 class Schedule:
-    @app.route('/api/schedule/<doc_id>', methods=['POST'])
-    @app.route('/api/schedule', methods=['POST'], defaults={'doc_id': None})
+
+    def _pop_id(payload):
+        if 'id' in payload:
+            payload.pop('id')
+        return payload
+
+    @app.route('/api/schedule/task/<doc_id>', methods=['POST'])
+    @app.route('/api/schedule/task', methods=['POST'], defaults={'doc_id': None})
     def post(doc_id):
-        payload = request.json if request.json else config[0]
+        payload = request.json
+        payload = Schedule._pop_id(payload)
         if not doc_id:
             result = TASK.insert(payload)
         else:
             result = TASK.update(payload, doc_ids=[int(doc_id)])
         return f'shedule put {result}', 200 
-    @app.route('/api/schedule', methods=['DELETE'])
-    def delete():
-        return 'shedule delete', 200 
-    @app.route('/api/schedule', methods=['GET'])
+    @app.route('/api/schedule/task/<doc_id>', methods=['DELETE'])
+    def delete(doc_id):
+        result = TASK.remove(doc_ids=[int(doc_id)])
+        return f'shedule delete {result}', 200 
+    @app.route('/api/schedule/all_task', methods=['GET'])
     def get():
         res = TASK.all()
+        for r in res:
+            r['id']=r.doc_id
         return jsonify(res), 200 
 
 
