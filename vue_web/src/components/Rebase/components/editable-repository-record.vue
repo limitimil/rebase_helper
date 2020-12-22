14e1349145e74b02a91a615ab2cd2636fd3b784d
limitimil <liminchien@cybersoft4u.com>
2020-12-22 07:03:32 -0800

    VUE: err:
    VUE: toggle mode, tooltip
    VUE: err: toggle
    VUE: err: use tooltip
    VUE: err: bugfix, refactor, styling
    VUE: err: refactor with vm as this
    VUE: err: refine styling
    VUE: err: auto element select, plugin panel ux
    VUE: err: use auto eleemnt select
    VUE: err: plugin panel will be hided when switch to edit mode
    VUE: err: value sync

--- vue_web/src/components/Rebase/components/editable-repository-record.vue
+++ vue_web/src/components/Rebase/components/editable-repository-record.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="editable-repository-record-root hack1">
-    <div class="operation d-inline-block hack2 ">
-      <div class="btn">{{operation}}</div>
+  <div class="editable-repository-record-root d-flex">
+    <div class="operation d-inline hack2 " @click="toggle">
+      <!-- TODO: make font awsome icon as a component to save typing-->
+      <i class="btn" :class="classOperationIcon"/>
     </div>
-    <div class="content d-inline-block hack3">
+    <div class="content hack3">
       <div class="fields-wrapper" :class="classFieldsWrapper">
         <div class="fields" :class="classFields">
-          <label for="">repository url</label>
-          <div class="content">{{url}}</div>
-          <Input class="content" v-model="url"></Input>
+          <label class="d-inline" for="">repository url</label>
+          <TextTruncateTooltip :content="url" class="field-content" v-if="flagMode.View"/>
+          <Input class="field-content" v-model="url" v-if="flagMode.Edit"></Input>
         </div>
         <div class="fields" :class="classFields">
-          <label for="">branches</label>
+          <label class="d-inline" for="">branches</label>
           <!-- TODO: should be filtered to be comma seperate format-->
-          <div class="content">{{ branches }}</div>
-          <Select class="content" v-model="branches"></Select>
+          <div class="field-content" v-if="flagMode.View">{{ branches }}</div>
+          <AutoElementSelect class="field-content" :value.sync="branches" v-if="flagMode.Edit"></AutoElementSelect>
         </div>
-        <div class="fields" :class="classFields">
-          <label for="">plugins</label>
+        <div class="fields" :class="classFields" v-if="flagMode.Edit">
+          <label class="d-inline" for="">plugins</label>
           <!-- TODO: should be filtered to be formated json-->
-          <div class="content">{{plugins}}</div>
-          <Input class="content" v-model="plugins"></Input>
+          <Input class="field-content" v-model="plugins" :disabled="!flagMode.Edit" :readonly="!flagMode.Edit" type="textarea"></Input>
         </div>
       </div>
     </div>
@@ -28,10 +28,12 @@
 </template>
 <script lang="ts">
 import Vue, { PropType } from 'vue';
+import TextTruncateTooltip from '@/widgets/tooltip';
+import AutoElementSelect from '@/widgets/select';
 
 enum EnumMode{
-  Edit='edit',
-  View='view',
+  Edit,
+  View
 };
 
 export default Vue.extend({
@@ -41,16 +43,51 @@ export default Vue.extend({
     branches: { type: Array, default: () => [] },
   }, 
   components: {
+    TextTruncateTooltip,
+    AutoElementSelect
   },
   data() {
     return {
       helloworold: 'helloworld' as string,
-      operation: EnumMode.View as EnumMode,
+      mode: EnumMode.View as EnumMode,
     };
   },
+  methods: {
+    toggle() {
+      const vm: any=this;
+      switch(vm.mode){
+        case EnumMode.Edit:
+          vm.mode = EnumMode.View;
+          break;
+        case EnumMode.View:
+          vm.mode = EnumMode.Edit;
+          break;
+      }
+    }
+  },
   computed: {
+    flagMode() {
+      const vm: any=this;
+      return {
+        Edit: vm.mode === EnumMode.Edit,
+        View: vm.mode === EnumMode.View,
+      };
+    },
+    classOperationIcon() {
+      const vm: any=this;
+      if (vm.mode === EnumMode.Edit) {
+        return 'fas fa-save';
+      }
+      return 'fas fa-edit';
+    },
     classFields() {
-      return 'col-4';
+      const vm: any=this;
+      switch(vm.mode){
+        case EnumMode.Edit:
+          return 'd-flex col-4';
+        case EnumMode.View:
+          return 'd-flex col-6';
+      }
     },
     classFieldsWrapper() {
       return 'row';
@@ -73,6 +110,14 @@ export default Vue.extend({
 }
 .hack5{
  background-color: hsla(240, 7%, 47%, 1);
+}
+.fields {
+  *{
+    padding-right: 1em;
+  }
+}
+.content {
+  width: 100%;
 }
 </style>
 
