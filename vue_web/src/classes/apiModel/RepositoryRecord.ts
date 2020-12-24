77db075a0c844d797977864aa7e1ee537839bd7c
limitimil <liminchien@cybersoft4u.com>
2020-12-24 05:08:08 -0800

    MODEL: rr: bugfix

--- vue_web/src/classes/apiModel/RepositoryRecord.ts
+++ vue_web/src/classes/apiModel/RepositoryRecord.ts
@@ -1,8 +1,8 @@
 export default class RepositoryRecord{
 
   public id: string | null = null;
-  public url: string;
-  public repository_url: string[] = [];
+  public repository_url: string;
+  public branches: string[] = [];
   public plugins: any = {};
   constructor(fields?: any) {
     if (fields) {
